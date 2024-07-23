a986060106eb5b47b1b8abc7447f5df62b69d53f
Edson Michaque <edson@michaque.com>
2024-07-23 20:45:00 +0200

    Add subject to license

--- generate.go
+++ generate.go
@@ -1,55 +1,56 @@
 package licensing
 
 import (
-    "crypto/rand"
-    "crypto/rsa"
-    "crypto/sha256"
-    "crypto/x509"
-    "encoding/base64"
-    "encoding/pem"
-    "encoding/json"
-    "errors"
-    "fmt"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"fmt"
 )
 
 type License struct {
-    ID           string            `json:"id"`
-    Type         string            `json:"type"`
-    ExpiresAt    string            `json:"expires_at"`
-    IssuedAt     string            `json:"issued_at"`
-    Issuer       string            `json:"issuer"`
-    Audience     []string          `json:"audience"`
-    Features     []string          `json:"features"`
-    Restrictions map[string]string `json:"restrictions"`
-    Metadata     map[string]string `json:"metadata"`
+	ID           string            `json:"id"`
+	Type         string            `json:"type"`
+	Subject      string            `json:"subject"`
+	ExpiresAt    string            `json:"expires_at"`
+	IssuedAt     string            `json:"issued_at"`
+	Issuer       string            `json:"issuer"`
+	Audience     []string          `json:"audience"`
+	Features     []string          `json:"features"`
+	Restrictions map[string]string `json:"restrictions"`
+	Metadata     map[string]string `json:"metadata"`
 }
 
 func Generate(privateKey *rsa.PrivateKey, data License) (string, error) {
-    dataJSON, err := json.Marshal(data)
-    if err != nil {
-        return "", err
-    }
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
 
-    hashed := sha256.Sum256(dataJSON)
-    signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, 0, hashed[:])
-    if err != nil {
-        return "", err
-    }
+	hashed := sha256.Sum256(dataJSON)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, 0, hashed[:])
+	if err != nil {
+		return "", err
+	}
 
-    licenseKey := fmt.Sprintf("%s.%s", base64.StdEncoding.EncodeToString(dataJSON), base64.StdEncoding.EncodeToString(signature))
-    return licenseKey, nil
+	licenseKey := fmt.Sprintf("%s.%s", base64.StdEncoding.EncodeToString(dataJSON), base64.StdEncoding.EncodeToString(signature))
+	return licenseKey, nil
 }
 
 func GenerateFromString(privateKeyPEM string, data License) (string, error) {
-    block, _ := pem.Decode([]byte(privateKeyPEM))
-    if block == nil || block.Type != "RSA PRIVATE KEY" {
-        return "", errors.New("failed to decode PEM block containing private key")
-    }
+	block, _ := pem.Decode([]byte(privateKeyPEM))
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return "", errors.New("failed to decode PEM block containing private key")
+	}
 
-    privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-    if err != nil {
-        return "", err
-    }
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
 
-    return Generate(privateKey, data)
+	return Generate(privateKey, data)
 }
